300c4ab17d6f18b9417b2e247b9b705894805c3d
Bojan Mirkovic <bojanmirkovic81@gmail.com>
2025-01-15 12:00:27 +0100

    ProductRepository implementation is finished. Next  to be done is to register Repository in the DependencyInjection

--- ProductApi.Infrastructure/Repositories/ProductRepository.cs
+++ ProductApi.Infrastructure/Repositories/ProductRepository.cs
@@ -2,6 +2,7 @@
 
 using eCommerce.SharedLibrary.LogFolder;
 using eCommerce.SharedLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
 using ProductApi.Application.Interfaces;
 using ProductApi.Domain.EntityModels;
 using ProductApi.Infrastructure.Database;
@@ -11,10 +12,28 @@ namespace ProductApi.Infrastructure.Repositories
 {
     public class ProductRepository(ProductDbContext context) : IProduct
     {
-        public Task<Response> CreateAsync(ProductModel entity)
+        public async Task<Response> CreateAsync(ProductModel entity)
         {
             try
             {
+                //check if the product slready exist
+                var getProduct = await GetByAsync(product => product.Name!.Equals(entity.Name));
+                if (getProduct is not null && !string.IsNullOrEmpty(getProduct.Name))
+                {
+                    return new Response(false, $"{entity.Name} is already added");
+                }
+
+                var currentEntity = context.Products.Add(entity).Entity;
+                await context.SaveChangesAsync();
+
+                if (currentEntity is not null && currentEntity.Id > 0)
+                {
+                    return new Response(true, $"{entity.Name} product successfuly added to database.");
+                }
+                else 
+                {
+                    return new Response(false, $"Error occured while adding {entity.Name} to database.");
+                }
 
             }
             catch (Exception ex)
@@ -22,35 +41,109 @@ namespace ProductApi.Infrastructure.Repositories
                 //Log the original exception
                 LogException.LogExceptions(ex);
 
-                //Display scary-free message to the user
-                return new Response(false, "Error occured adding new product.");
-                
+                // Display a user-friendly error message
+                return new Response(false, "Error occurred adding new product.");
             }
         }
 
-        public Task<Response> DeleteAsync(ProductModel entity)
+        public async Task<Response> DeleteAsync(ProductModel entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var product = await FindByIdAsync(entity.Id);
+                if (product is null)
+                {
+                    return new Response(false, $"{entity.Name} not found");
+                }
+                context.Products.Remove(product);
+                await context.SaveChangesAsync();
+                return new Response(true, $"{entity.Name} is deleted successfully.");
+            }
+            catch (Exception ex)
+            {
+                //Log the original exception
+                LogException.LogExceptions(ex);
+
+                // Display a user-friendly error message
+                return new Response(false, "Error occurred deleting product.");
+            }
         }
 
-        public Task<IEnumerable<ProductModel>> GetaAllAsync()
+        public async Task<ProductModel?> FindByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var product = await context.Products.FindAsync(id);
+                return product;
+            }
+            catch (Exception ex)
+            {
+                //Log the original exception
+                LogException.LogExceptions(ex);
+
+                // Display a user-friendly error message
+                throw new Exception("Error occured retriving product");
+            }
         }
 
-        public Task<ProductModel> GetByAsync(Expression<Func<ProductModel, bool>> predicate)
+        public async Task<IEnumerable<ProductModel>> GetaAllAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var products = await context.Products.AsNoTracking().ToListAsync();
+                return products;
+            }
+            catch (Exception ex)
+            {
+                //Log the original exception
+                LogException.LogExceptions(ex);
+
+                // Display a user-friendly error message
+                throw new Exception("Error occured while retriving all products");
+            }
         }
 
-        public Task<ProductModel> GetByIdAsync(int id)
+        public async Task<ProductModel?> GetByAsync(Expression<Func<ProductModel, bool>> predicate)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var product = await context.Products.AsNoTracking().FirstOrDefaultAsync(predicate);
+                return product;
+            }
+            catch (Exception ex)
+            {
+                //Log the original exception
+                LogException.LogExceptions(ex);
+
+                // Display a user-friendly error message
+                throw new Exception("An error occurred while retrieving the product.");
+            }
         }
 
-        public Task<Response> UpadteAsync(ProductModel entity)
+        public async Task<Response> UpadteAsync(ProductModel entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var existingProduct = await FindByIdAsync(entity.Id);
+                if (existingProduct is null) 
+                {
+                    return new Response(false, $"{entity.Name} not found");
+                }
+
+                context.Entry(existingProduct).State = EntityState.Detached;
+                context.Products.Update(entity);
+                await context.SaveChangesAsync();
+                return new Response(true, $"{entity.Name} is updated successfully.");
+
+            }
+            catch (Exception ex)
+            {
+                //Log the original exception
+                LogException.LogExceptions(ex);
+
+                // Display a user-friendly error message
+                return new Response(false,"An error occurred while updating the existing product.");
+            }
         }
     }
 }
